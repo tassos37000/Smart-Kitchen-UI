1ac2e287d3b9b9dabcc12a0bdf54eb6367ed3e2f
ΑΝΑΣΤΑΣΙΟΣ ΑΓΓΕΛΙΔΗΣ <tassos37000@gmail.com>
2023-04-05 21:53:27 +0300

    create android studio project

---
+++ app/src/main/java/com/example/smartkitchen/MainScreen.java
@@ -0,0 +1,13 @@
+package com.example.smartkitchen;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
