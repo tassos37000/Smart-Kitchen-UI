fbb3fe71584afb6f9535bbff6a50c08b6b2534b9
ΑΝΑΣΤΑΣΙΟΣ ΑΓΓΕΛΙΔΗΣ <tassos37000@gmail.com>
2023-04-07 00:16:00 +0300

    keep state of switch and show correct image in SettingsActivity

--- app/src/main/java/com/example/smartkitchen/MainScreenActivity.java
+++ app/src/main/java/com/example/smartkitchen/MainScreenActivity.java
@@ -4,29 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageButton;
-import android.view.View;
 
-public class MainScreen extends AppCompatActivity {
-
+public class MainScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
         ImageButton settingsButton = findViewById(R.id.settings);
 
-        settingsButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(MainScreen.this, SettingsActivity.class);
-                startActivity(intent);
-            }
+        settingsButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainScreenActivity.this, SettingsActivity.class);
+            startActivity(intent);
         });
     }
-
-    @Override
-    protected void onPause(){
-
-    }
-
-
 }
--- app/src/main/java/com/example/smartkitchen/SettingsActivity.java
+++ app/src/main/java/com/example/smartkitchen/SettingsActivity.java
@@ -1,82 +1,68 @@
 package com.example.smartkitchen;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.CompoundButton;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Switch;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity{
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        Switch voiceCommandsSwitch = findViewById(R.id.voiceCommandSwitch);
-        ImageView voiceCommandImage = findViewById(R.id.imageVoiceComand);
+        SwitchCompat voiceCommandsSwitch = findViewById(R.id.voiceCommandSwitch);
+        ImageView voiceCommandImage = findViewById(R.id.imageVoiceCommand);
+        changeSaveSwitchImage(voiceCommandsSwitch, voiceCommandImage, "VoiceCommandSS", R.drawable.microphone, R.drawable.microphone_mute);
 
-        if (savedInstanceState != null) {
-            boolean switchState = savedInstanceState.getBoolean("switch_VC_state");
-            voiceCommandsSwitch.setChecked(switchState);
-        }
-
-        voiceCommandsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    voiceCommandImage.setImageResource(R.drawable.microphone);
-                } else {
-                    voiceCommandImage.setImageResource(R.drawable.microphone_mute);
-                }
-            }
-        });
-
-        Switch voiceResponseSwitch = findViewById(R.id.voiceResponsesSwitch);
+        SwitchCompat voiceResponseSwitch = findViewById(R.id.voiceResponsesSwitch);
         ImageView voiceResponseImage = findViewById(R.id.imageVoiceResponse);
+        changeSaveSwitchImage(voiceResponseSwitch, voiceResponseImage, "VoiceResponseSS", R.drawable.sound, R.drawable.mute_sound);
 
-        voiceResponseSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    voiceResponseImage.setImageResource(R.drawable.sound);
-                } else {
-                    voiceResponseImage.setImageResource(R.drawable.mute_sound);
-                }
-            }
-        });
-
-        Switch notificationSwitch = findViewById(R.id.notificationSwitch);
+        SwitchCompat notificationSwitch = findViewById(R.id.notificationSwitch);
         ImageView notificationImage = findViewById(R.id.imageNotification);
-
-        notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    notificationImage.setImageResource(R.drawable.bell);
-                } else {
-                    notificationImage.setImageResource(R.drawable.mute_bell);
-                }
-            }
-        });
+        changeSaveSwitchImage(notificationSwitch, notificationImage, "NotificationSS", R.drawable.bell, R.drawable.mute_bell);
 
         ImageButton backButton = findViewById(R.id.backButton);
 
-        backButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(SettingsActivity.this, MainScreen.class);
-                startActivity(intent);
-            }
+        backButton.setOnClickListener(view -> {
+            Intent intent = new Intent(SettingsActivity.this, MainScreenActivity.class);
+            startActivity(intent);
         });
-
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Switch voiceCS = findViewById(R.id.voiceCommandSwitch);
-        boolean switchVCState = voiceCS.isChecked();
-        outState.putBoolean("switch_VC_state", switchVCState);
+    /**
+     * @param toggleSwitch
+     * @param imageView
+     * @param key
+     * @param imageOn
+     * @param imageOff
+     */
+    private void changeSaveSwitchImage(SwitchCompat toggleSwitch, ImageView imageView, String key, int imageOn, int imageOff){
+        boolean SwitchState = sharedPreferences.getBoolean(key, true);
+        Log.i(key, String.valueOf(SwitchState));
+        toggleSwitch.setChecked(SwitchState);
+        if (SwitchState) {
+            imageView.setImageResource(imageOn);
+        } else {
+            imageView.setImageResource(imageOff);
+        }
+        toggleSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(key, isChecked);
+            editor.apply();
+            if (isChecked) {
+                imageView.setImageResource(imageOn);
+            } else {
+                imageView.setImageResource(imageOff);
+            }
+        });
     }
 }
